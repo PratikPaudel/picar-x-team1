57f42c29c46104396d79dc4ce4d2c68586948bf9
Pratik Paudel <56014021+PratikPaudel@users.noreply.github.com>
2023-11-11 12:49:49 -0600

    Update picar_x-server.py
    conflicts with the move and server-client interaction, need the while true function to be corrected?

--- picar_x-server.py
+++ picar_x-server.py
@@ -1,10 +1,54 @@
 from socket import *
+from picarx import Picarx
+import time
 
-mySock = socket(AF_INET,SOCK_DGRAM)
+
+mySock = socket(AF_INET, SOCK_DGRAM)
 mySock.bind(('', 25565))
 
-while True:
-    message, clientAddress = mySock.recvfrom(2048)
-    message = message.decode()
-    print(message)
-    mySock.sendto("got it".encode(), clientAddress)
+def move():
+    try:
+        px = Picarx()
+        px.forward(30)
+        time.sleep(0.5)
+        for angle in range(0,35):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35,-35,-1):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35,0):
+            px.set_dir_servo_angle(angle)
+            time.sleep(0.01)
+            px.forward(0)
+            time.sleep(1)
+        for angle in range(0,35):
+            px.set_camera_servo1_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35,-35,-1):
+            px.set_camera_servo1_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35,0):
+            px.set_camera_servo1_angle(angle)
+            time.sleep(0.01)
+        for angle in range(0,35):
+            px.set_camera_servo2_angle(angle)
+            time.sleep(0.01)
+        for angle in range(35,-35,-1):
+            px.set_camera_servo2_angle(angle)
+            time.sleep(0.01)        
+        for angle in range(-35,0):
+            px.set_camera_servo2_angle(angle)
+            time.sleep(0.01)
+            
+    finally:
+        px.stop()
+        time.sleep(.2)
+
+if __name__ == "__main__":
+    while True:
+        message, clientAddress = mySock.recvfrom(2048)
+        message = message.decode()
+        print(message)
+        mySock.sendto("got it".encode(), clientAddress)
+        move()

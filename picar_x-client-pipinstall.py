20859268de106a1b36d6f03c5a81c0468ef2bfe9
jazzncomics12 <143197393+jazzncomics12@users.noreply.github.com>
2023-11-28 18:50:44 -0600

    Turning Angles + Speed Input
    Tuned the turning and forward angles so that it travels in the expected directions and doesn't encounter issues with the turning angle being too sharp and interfering with movement. Additionally, added a prompt when the 'S' key is pressed that allows the user to change the speed of the robot between 0-100.
    
    Could not figure out how to clear the input field when asking for the speed, so the last answer (plus the 'S' that prompted the input) are recorded in the input field and must be cleared before entering the desired speed (is there a way to fix this?). We also tried to smooth out some of the car motion with questionable levels of success, so that it would behave more like a remote control car.

---
+++ picar_x-client-pipinstall.py
@@ -0,0 +1,51 @@
+from socket import *
+import keyboard
+
+yourSock = socket(AF_INET, SOCK_DGRAM)
+
+# Define functions to be called on key events
+def on_left_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        #print("sending command left:")
+        send_command('left')
+
+def on_right_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        send_command('right')
+
+def on_up_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        send_command('forward')
+
+def on_down_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        send_command('stop')
+
+def on_b_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        send_command('backward')
+
+def on_s_key(event):
+    if event.event_type == keyboard.KEY_DOWN:
+        send_command('speed')
+        speed_select()
+
+# Function to send commands to the server
+def send_command(msg):
+    yourSock.sendto(msg.encode(), ('10.53.20.60', 25565))
+
+# Function to change speed
+def speed_select():
+    speed = input("What speed would you like the robot to move (0-100): ")
+    yourSock.sendto(speed.encode(), ('10.53.20.60', 25565))
+
+# Bind the functions to the corresponding keys
+keyboard.on_press_key('left', on_left_key)
+keyboard.on_press_key('right', on_right_key)
+keyboard.on_press_key('up', on_up_key)
+keyboard.on_press_key('down', on_down_key)
+keyboard.on_press_key('B', on_b_key)  # 'B' key for backward
+keyboard.on_press_key('S', on_s_key)  # 'S' key for speed select
+
+# Keep the program running
+keyboard.wait()
